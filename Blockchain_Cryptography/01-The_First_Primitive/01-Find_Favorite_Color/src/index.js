ceece37b8eeebd31a86323cad84ae70038f9d8af
Vini Black <viniibl4ck@gmail.com>
2024-04-09 10:59:38 -0300

    aula 01

--- Blockchain_Cryptography/01-The_First_Primitive/01-Find_Favorite_Color/src/index.js
+++ Blockchain_Cryptography/01-The_First_Primitive/01-Find_Favorite_Color/src/index.js
@@ -6,7 +6,20 @@ const COLORS = ['red', 'green', 'blue', 'yellow', 'pink', 'orange'];
 
 // dado um hash, retorna a cor que criou o hash
 function findColor(hash) {
-    
+  return COLORS.find(color => toHex(sha256(utf8ToBytes(color))) === hash);
 }
+
+const red = "red"
+console.log("string: " )
+console.log(red)
+console.log("utf8ToBytes:")
+console.log(utf8ToBytes(red))
+console.log("utf8ToBytes + sha256:")
+console.log(sha256(utf8ToBytes(red)))
+console.log("toHex")
+console.log(toHex(sha256(utf8ToBytes(red))))
+console.log("findColor")
+console.log(findColor(toHex(sha256(utf8ToBytes(red)))))
+
 
 module.exports = findColor;
