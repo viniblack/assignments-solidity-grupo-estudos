aa54f2ce7c04b3feecda916afb21d2f1374ba98f
Vini Black <viniibl4ck@gmail.com>
2024-04-30 13:58:03 -0300

    resolvendo tudo aula 2

--- Blockchain_Cryptography/02-Digital_Signatures/04-Key_to_Address/src/address.js
+++ Blockchain_Cryptography/02-Digital_Signatures/04-Key_to_Address/src/address.js
@@ -8,21 +8,30 @@ const { keccak256 } = require("ethereum-cryptography/keccak");
 async function address(message, signature, recoveryBit) {
   const publicKey = await secp.recoverPublicKey(hashMessage(message), signature, recoveryBit);
 
+  const chavePublica = toHex(publicKey)
+  console.log('publicKey');
+  console.log(chavePublica);
+
   const hashKey = keccak256(publicKey.slice(1, publicKey.length))
   console.log('hashKey');
   console.log(hashKey);
+
+  // const key2 = hashKey.slice(0, 12);
+  // console.log('key2');
+  // console.log(key2);
+
   const key = hashKey.slice(-20);
   console.log('key');
   console.log(key);
 
   console.log('toHex key');
-  console.log(`${toHex(key)}`);
+  console.log(`0x${toHex(key)}`);
 }
 
 async function test() {
   const [sig, recoveryBit] = await signMessage('hello world');
 
-  address('hello world', sig, recoveryBit)  
+  address('hello world', sig, recoveryBit)
 }
 
 test()
